7327fda26fc5600a475fa59472ce6d34a275c63e
Dariusz Gertych <dariusz.gertych@gmail.com>
2014-05-28 15:46:06 +0200

    Update swagger-ui to 2.0.17

--- app/assets/javascripts/grape_swagger_rails/application.js
+++ app/assets/javascripts/grape_swagger_rails/application.js
@@ -1,7 +1,7 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+// This is a manifest file that'll be compiled into application, which will include all the files
 // listed below.
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// Any JavaScript/Coffee file within this directory, assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
@@ -21,4 +21,4 @@
 //= require ./swagger
 //= require ./swagger-ui
 //= require ./highlight.7.3.pack
-//= require ./base64
+//= require ./swagger-oauth

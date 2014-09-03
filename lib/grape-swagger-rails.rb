a6db75ddc04ab706129f9c941077698d37e280f0
Dariusz Gertych <dariusz.gertych@gmail.com>
2014-09-03 16:20:37 +0200

    Add possibility to add multiple api_keys and hide application url
    changed :authentication_proc to before_filter_proc:
    Conflicts:
    	app/views/grape_swagger_rails/application/index.html.erb
    	lib/grape-swagger-rails.rb

--- lib/grape-swagger-rails.rb
+++ lib/grape-swagger-rails.rb
@@ -18,14 +18,14 @@ module GrapeSwaggerRails
     url:                  '/swagger_doc',
     app_name:             'Swagger',
     app_url:              'http://swagger.wordnik.com',
+    hide_app_url:         false,
 
     headers:              {},
 
-    api_auth:             '',        # 'basic' or 'bearer'
-    api_key_name:         'api_key', # 'Authorization'
-    api_key_type:         'query',   # 'header'
+    api_auth:             '', # 'basic'
+    api_keys:             [],
 
-    before_filter_proc:   nil # Proc used as a controller before filter
+    before_filter_proc:  nil # Proc used as a controller before filter that returns a boolean
   )
 
 end
